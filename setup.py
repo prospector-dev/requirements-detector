0f41d1d73b123a94fd4a779fceb3ad9c0daedad3
carlio <git@carlcrowder.com>
2013-10-17 11:08:15 +0200

    Making a fixed version for initial release

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 # -*- coding: UTF-8 -*-
 from distutils.core import setup
 from setuptools import find_packages
-import time
 
 
-_version = "0.1.dev%s" % int(time.time())
+_version = "0.1"
 _packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
 
 _short_description = "Python tool to find and list requirements of a Python project"
@@ -18,7 +17,7 @@ setup(
     description=_short_description,
     version=_version,
     scripts=['bin/detect-requirements'],
-    install_requires=['astroid'],
+    install_requires=['astroid>=1.0.0'],
     packages=_packages,
     license='MIT',
     keywords=('python requirements detector',)

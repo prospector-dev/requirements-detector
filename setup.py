3834fd91492944bc6db7863ce915f26322afbd2b
carlio <git@carlcrowder.com>
2013-10-21 07:52:35 +0200

    Setting version back to 0.1 after stupid merge

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 # -*- coding: UTF-8 -*-
 from distutils.core import setup
 from setuptools import find_packages
-import time
 
 
-_version = "0.1.dev%s" % int(time.time())
+_version = "0.1"
 _packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
 
 _short_description = "Python tool to find and list requirements of a Python project"
